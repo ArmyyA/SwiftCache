4b776ed7618a443b1552bfff16dc284f2a05c126
Arham Ansari <73043239+ArmyyA@users.noreply.github.com>
2023-08-04 18:10:18 -0400

    Initialize server main function.

--- src/CacheServer.java
+++ src/CacheServer.java
@@ -1,3 +1,22 @@
+import java.io.*;
+import java.net.*;
+
 public class CacheServer {
-    
+    public static void main(String[] args) {
+        int port = 161;
+
+        // Establish the server socket listening at port 161
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is running on port: " + port);
+
+            while (true) {
+                // Accepts when client reaches out to server
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket.getInetAddress());
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
